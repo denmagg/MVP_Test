b2e534f2551c70389851d409fec751bf7c76e5fb
Denis <denmagg.work@gmail.com>
2021-10-05 13:27:30 +0300

    Initial Commit

---
+++ MVP-2N/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVP-2N
+//
+//  Created by Denis Medvedev on 05/10/2021.
+//  Copyright © 2021 Denis Medvedev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MVP-2NTests/MVP_2NTests.swift
@@ -0,0 +1,34 @@
+//
+//  MVP_2NTests.swift
+//  MVP-2NTests
+//
+//  Created by Denis Medvedev on 05/10/2021.
+//  Copyright © 2021 Denis Medvedev. All rights reserved.
+//
+
+import XCTest
+@testable import MVP_2N
+
+class MVP_2NTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
